747a2b734e41074afb0328f93a3380100a04d7f4
YizheC <cheng256@wisc.edu>
2021-12-06 18:35:52 -0600

    miestone2 part D

--- vocabulary_size_postgres.py
+++ vocabulary_size_postgres.py
@@ -7,6 +7,8 @@ cur=conn.cursor()
 cur.execute('''
 select phrase, cur_time, num_cur_min_p from phrases where len_word = 1 group by (phrase,cur_time)
 ''')
+rs=cur.fetchall() 
+for r in rs: print (r)
 
 conn.commit() 
 cur.close()
